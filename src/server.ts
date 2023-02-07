b5b4f82ff2b0d56f2fdee266cf84e717db50a2fc
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-02-07 19:02:16 -0300

    chore(server): add database connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import app from "./utils/app";
-import logger from "./utils/logger"
+import logger from "./utils/logger";
+import Connection from "./utils/connect-aws-rds";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => logger.info("Server running on http://localhost:3000"));
+app.listen(PORT, async () => {
+  logger.info("Server running on http://localhost:3000");
+  await Connection.execute();
+});
