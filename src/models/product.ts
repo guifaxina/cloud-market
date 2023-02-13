5466f270b7e9d9d18618009060071e7f847ac1e6
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-02-13 12:56:33 -0300

    chore(models): create product model

---
+++ src/models/product.ts
@@ -0,0 +1,23 @@
+import { DataTypes, Sequelize } from "sequelize";
+
+const sequelize = new Sequelize();
+
+const userModel = sequelize.define("User", {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  title: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING, allowNull: false },
+  img: { type: DataTypes.STRING, allowNull: false },
+  categories: {
+    type: DataTypes.ENUM(
+      "sneakers",
+      "accessories",
+      "shirts",
+      "jackets",
+      "trousers"
+    ),
+    allowNull: false,
+  },
+  size: { type: DataTypes.STRING },
+  color: { type: DataTypes.STRING },
+  price: { type: DataTypes.DECIMAL(6, 2), allowNull: false },
+});
